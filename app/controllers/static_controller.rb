6e1c7f4ddec746f2ce0f63d47cea875582f97c14
claudiu <claudiu.garba@gmail.com>
2014-07-25 22:47:38 +0200

    stuff

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,4 +6,13 @@ class StaticController < ApplicationController
   def stocks
     render json: Stock.all
   end
+
+  def create
+    render json: Stock.create(stock_params)
+  end
+
+  private
+  def stock_params
+    params.require(:stock).permit(:symbol, :name, :bid, :ask, :year_low, :year_high)
+  end
 end
